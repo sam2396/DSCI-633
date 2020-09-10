8a6aef7bfe1bc470e9f1a2a344650e104ee40ded
Sanket Waghmare <sawaghmare23@gmail.com>
2020-09-11 02:54:30 +0530

    Homework Done

--- assignments/assignment3/my_NB.py
+++ assignments/assignment3/my_NB.py
@@ -1,36 +1,62 @@
 import pandas as pd
+import numpy as np
+from collections import Counter
 
 class my_NB:
 
     def __init__(self, alpha=1):
         # alpha: smoothing factor
         # P(xi = t | y = c) = (N(t,c) + alpha) / (N(c) + n(i)*alpha)
-        # where n(i) is the number of available categories of feature i
+        # where n(i) is the number of available categories (values) of feature i
         # Setting alpha = 1 is called Laplace smoothing
         self.alpha = alpha
 
     def fit(self, X, y):
+        self.classes_ = list(set(list(y)))
+        pairs = {}
         # X: pd.DataFrame, independent variables, str
         # y: list, np.array or pd.Series, dependent variables, int or str
-        self.classes_ = list(set(list(y)))
-        # write your code below
+        # list of classes for this model
+        #self.classes_ = list(set(list(y)))
+        # for calculation of P(y)
+        self.P_y = Counter(y)
+        # self.P[yj][Xi][xi] = P(xi|yi) where Xi is the feature name and xi is the feature value, yj is a specific class label
+        self.P = {}
+        xmain = {}
+        for key in X:
+            xmain[key] = set(X[key])
+        for label in self.classes_:
+            self.P[label] = {}
+            for key in X:
+                self.P[label][key] = {}
+                total = Counter(X[key].where(label==y))
+                for value in xmain[key]:
+                    self.P[label][key][value]=(total[value]+self.alpha)/(self.P_y[label]+len(xmain[key])*self.alpha)
         return
-
-    def predict(self, X):
-        # X: pd.DataFrame, independent variables, str
-        # return predictions: list
-        # write your code below
-        return predictions
-
     def predict_proba(self, X):
         # X: pd.DataFrame, independent variables, str
         # prob is a dict of prediction probabilities belonging to each categories
         # return probs = pd.DataFrame(list of prob, columns = self.classes_)
-        # write your code below
-
-        ##################
-        assert (all(probs.keys() == self.classes_))
+        probs = {}
+        for label in self.classes_:
+            p = self.P_y[label]
+            for key in X:
+                p *= X[key].apply(lambda value: self.P[label][key][value] if value in self.P[label][key] else 1)
+            probs[label] = p
+        probs = pd.DataFrame(probs, columns=self.classes_)
+        sums = probs.sum(axis=1)
+        probs = probs.apply(lambda v: v / sums)
         return probs
+
+    def predict(self, X):
+        # X: pd.DataFrame, independent variables, str
+        # return predictions: list
+        probs = self.predict_proba(X)
+        predictions = [self.classes_[np.argmax(prob)] for prob in probs.to_numpy()]
+        return predictions
+
+
+
 
 
 
