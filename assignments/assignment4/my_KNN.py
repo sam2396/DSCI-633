9e866f82a041ead558566b3f9a37db25b86a1abc
Sanket Waghmare <sawaghmare23@gmail.com>
2020-09-15 23:35:37 +0530

    Updated

--- assignments/assignment4/my_KNN.py
+++ assignments/assignment4/my_KNN.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 
-class my_KNN:
+class my_KNN_hint:
 
     def __init__(self, n_neighbors=5, metric="minkowski", p=2):
         # metric = {"minkowski", "euclidean", "manhattan", "cosine"}
@@ -27,36 +27,24 @@ class my_KNN:
             finaldist1 = np.empty
             c = np.array(self.X)
             for i in range(len(c)):
-                dist = (sum((abs(c[i]-x))**2))**(1/2)
+                dist = (sum((abs(c[i]-x))**self.p))**(1/self.p)
                 finaldist1 = np.append(finaldist1,dist)
                 #Final minkowski distance  
             finaldist1 = np.delete(finaldist1,[0])
             distances = finaldist1
+            return distances
 
 
         elif self.metric == "euclidean":
             distances = "write your own code"
-            #for i in range(len(c)):
-                #dist = np.linalg.norm(c[i]-b)
-                #finaldist = np.append(finaldist,dist)
-                #Final euclidean distance
 
 
         elif self.metric == "manhattan":
             distances = "write your own code"
-            #dist = 0
-            #for i in range(len(c)):
-            #dist = (sum((abs(c[i]-b))))
-            #print(dist)
-            #Final manhattan distance  
 
 
         elif self.metric == "cosine":
             distances = "write your own code"
-          #  for i in range(len(c)):
-                #dist = (abs(sum(c[i]*b)))/(sqrt(sum(c[i]**2))*sqrt(sum(b**2)))
-                #print(dist)
-                #Final Cosine distance
 
 
         else:
@@ -68,14 +56,19 @@ class my_KNN:
         # Output: Counter(labels of the self.n_neighbors nearest neighbors)
         distances = self.dist(x)
         
-        distances2 = np.sort(distances)
-        distances3 = distances2[:5]
-        
-        output = distances3
+        #distances2 = np.sort(distances)
+        #distances3 = distances2[:5]
+        p1 = np.argsort(distances)
+        p2 = p1[:5]
+        p3 = []
+        for i in p2:
+            p3.append(self.y[i])
+        output1 = Counter(p3)
+        #output = distances3
 
 
 
-        return output
+        return output1
 
     def predict(self, X):
         # X: pd.DataFrame, independent variables, float
@@ -93,11 +86,12 @@ class my_KNN:
             X_feature = X[self.X.columns]
         except:
             raise Exception("Input data mismatch.")
-
+        
         for x in X_feature.to_numpy():
             neighbors = self.k_neighbors(x)
             probs.append({key: neighbors[key] / float(self.n_neighbors) for key in self.classes_})
         probs = pd.DataFrame(probs, columns=self.classes_)
         return probs
+
 
 
