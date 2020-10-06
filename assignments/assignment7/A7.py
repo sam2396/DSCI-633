55c9fc03816fd780a6962b23de3d2bd92bbea519
Sanket Waghmare <sawaghmare23@gmail.com>
2020-10-06 22:56:44 +0530

    Updated

--- assignments/assignment7/A7.py
+++ assignments/assignment7/A7.py
@@ -1,44 +1,41 @@
-import my_preprocess
+import my_preprocess_hint
 import pandas as pd
 from collections import Counter
-####### Add assignment2 to sys.path to import my_DT #######
-import sys
-sys.path.insert(0,'../assignment2')
-from my_DT import my_DT
+from sklearn.tree import DecisionTreeClassifier
+
 ##################################################
 
 if __name__ == "__main__":
     # Load training data
     data_train = pd.read_csv("../data/Iris_train.csv")
     # Separate independent variables and dependent variables
-    independent = ["SepalLengthCm",	"SepalWidthCm",	"PetalLengthCm", "PetalWidthCm"]
+    independent = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]
     X = data_train[independent]
     y = data_train["Species"]
     # Preprocess (train)
-    X_norm = my_preprocess.normalize(X)
-    X_pca = my_preprocess.pca(X_norm, n_components=2)
-    sample = my_preprocess.stratified_sampling(y, ratio = 0.5, replacement = False)
+    X_norm = my_preprocess_hint.normalize(X)
+    principal_components = my_preprocess_hint.pca(X_norm, n_components=2)
+    X_pca = X_norm.dot(principal_components)
+    sample = my_preprocess_hint.stratified_sampling(y, ratio=0.5, replace=False)
+
     X_sample = X_pca[sample]
-    y_sample = np.asarray(y)[sample]
+    y_sample = y[sample].to_numpy()
     print(X_sample)
     print(Counter(y_sample))
     print(Counter(y))
     # Fit model
-    clf = my_DT()
+    clf = DecisionTreeClassifier()
     clf.fit(X_sample, y_sample)
     # Load testing data
     data_test = pd.read_csv("../data/Iris_test.csv")
     X_test = data_test[independent]
     # Preprocess (test)
-    X_test_norm = my_preprocess.normalize(X_test)
-    X_test_pca = my_preprocess.pca(X_test_norm, n_components=2)
+    X_test_norm = my_preprocess_hint.normalize(X_test)
+    X_test_pca = X_test_norm.dot(principal_components)
     # Predict
     predictions = clf.predict(X_test_pca)
-    # Predict probabilities
-    probs = clf.predict_proba(X_test_pca)
-    # Print results
-    for i, pred in enumerate(predictions):
-        print("%s\t%f" % (pred, probs[pred][i]))
-    
-    
+    # Output predictions on test data
+    print(predictions)
+
+
 
--- assignments/assignment7/my_preprocess.py
+++ assignments/assignment7/my_preprocess.py
@@ -1,5 +1,8 @@
 import numpy as np
 from scipy.linalg import svd
+from copy import deepcopy
+from collections import Counter
+from pdb import set_trace
 
 def pca(X, n_components = 5):
     #  Use svd to perform PCA on X
@@ -7,10 +10,27 @@ def pca(X, n_components = 5):
     #     X: input matrix
     #     n_components: number of principal components to keep
     #  Output:
-    #     X_pca: output matrix of n_components columns (numpy.array)
-    #  Write your own code below:
+    #     principal_components: the top n_components principal_components
+    #     X_pca = X.dot(principal_components)
+
     U, s, Vh = svd(X)
-    return X_pca
+    V = Vh.transpose()
+    principal_components = V[:,:n_components]
+    return principal_components
+
+def vector_norm(x, norm="Min-Max"):
+    # Calculate the normalized vector
+    if norm == "Min-Max":
+        x_norm = (x-np.min(x))/(np.max(x)-np.min(x))
+    elif norm == "L1":
+        x_norm = x/(np.sum(x))
+    elif norm == "L2":
+        x_norm = x/np.sqrt(np.sum(x**2))
+    elif norm == "Standard_Score":
+        x_norm = (x-np.mean(x)/np.std(x))
+    else:
+        raise Exception("Unknown normlization.")
+    return x_norm
 
 def normalize(X, norm="Min-Max", axis = 1):
     #  Inputs:
@@ -20,19 +40,44 @@ def normalize(X, norm="Min-Max", axis = 1):
     #     axis = 1: normalize columns
     #  Output:
     #     X_norm: normalized matrix (numpy.array)
-    #  Write your own code below:
-    X_array = np.asarray(X)
+
+    X_norm = deepcopy(np.asarray(X))
+    m, n = X_norm.shape
+    if axis == 1:
+        for col in range(n):
+            X_norm[:,col] = vector_norm(X_norm[:,col], norm=norm)
+    elif axis == 0:
+        X_norm = np.array([vector_norm(X_norm[i], norm=norm) for i in range(m)])
+    else:
+        raise Exception("Unknown axis.")
     return X_norm
 
-def stratified_sampling(y, ratio, replacement = True):
+def stratified_sampling(y, ratio, replace = True):
     #  Inputs:
     #     y: class labels
     #     0 < ratio < 1: number of samples = len(y) * ratio
-    #     replacement = True: sample with replacement
-    #     replacement = False: sample without replacement
+    #     replace = True: sample with replacement
+    #     replace = False: sample without replacement
     #  Output:
     #     sample: indices of stratified sampled points
-    #                    (ratio is the same across each class)
-    #  Write your own code below:
+    #             (ratio is the same across each class,
+    #             samples for each class = int(np.ceil(ratio * # data in each class)) )
+
+    if ratio<=0 or ratio>=1:
+        raise Exception("ratio must be 0 < ratio < 1.")
     y_array = np.asarray(y)
-    return sample
+    dist = []
+    dist = list(set(y_array))
+    clusters = [[] for i in range(len(dist))]
+    for x in y_array:
+        for d in dist:
+            if(x == d):
+                clusters[dist.index(d)] = np.where(y_array==x)
+    fin = []
+    for n in range(len(clusters)):
+        tot = np.ceil(len(clusters[n][0])*ratio)
+        fin.append(np.random.choice(clusters[n][0],int(tot),replace=replace))
+    fin = np.concatenate((fin),axis=0)
+    sample = fin
+
+    return sample.astype(int)
